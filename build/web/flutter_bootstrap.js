f73ff3261161ae19c34278fbb6e8c1c97efa8b39
kimsoowan001-ai <kimsoowan001@gmail.com>
2026-08-31 05:22:01 +0900

    KT&G 스플래시 인트로 및 로그인 로고 화면 추가

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2719920967" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "155499866" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
